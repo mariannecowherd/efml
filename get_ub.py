5d9e19f7f42249c0d9ac1ce97ac4ceebf74bfb37
Marianne <mariannecowherd@gmail.com>
2020-12-22 21:40:30 -0500

    Update get_ub.py

--- get_ub.py
+++ get_ub.py
@@ -26,16 +26,7 @@ thetamaj_summer =  -28.4547
 dphi = np.pi/4 #Discretizing the phase
 phasebins = np.arange(-np.pi,np.pi,dphi)
 
-# profiles = np.zeros((30,len(burstnums),len(phasebins)))
-# uw_wave = np.zeros((30,len(burstnums),len(phasebins)))
-# upwp = np.zeros((30,len(burstnums),len(phasebins)))
-# z = np.zeros((30,len(burstnums)))
 
-#dissipation
-# epsilon = np.zeros((30,len(burstnums),len(phasebins)))
-# dudz = np.zeros((30,len(burstnums),len(phasebins)))
-# tke = np.zeros((30,len(burstnums),len(phasebins)))
-# tke_wave = np.zeros((30,len(burstnums),len(phasebins)))
 ub = np.zeros(len(burstnums))
 
 #this takes many hours to run -- load from .npy files if possible
@@ -47,30 +38,9 @@ for n in range(188,213):
         veleast,velnorth = vfs.pa_rotation(vec['velmaj'],vec['velmin'],-thetamaj_summer)
         velmajwave,velminwave = vfs.pa_rotation(veleast,velnorth,wavedir[n])
         u = np.nanmean(velmajwave[0:6,], axis = 0) #vertical average
-        #v = np.nanmean(velminwave[-5:,], axis=0) #vertical average
-
-        #Calculating fluctuating velocity
-        p,m = np.shape(velmajwave)
-        #up = np.zeros((p,m))
-        #vp = np.zeros((p,m))
-        #w1p = np.zeros((p,m))
-        #ubar = np.nanmean(velmajwave,axis = 1)
-        #vbar = np.nanmean(velminwave,axis = 1)
-        #w1bar = np.nanmean(vec['velz1'],axis = 1)
-        #for i in range(m):
-        #    up[:,i] = velmajwave[:,i] - ubar
-        #    vp[:,i] = velmajwave[:,i] - vbar
-        #    w1p[:,i] = vec['velz1'][:,i] - w1bar
-
-
-        #spectrum to find wave peak
-        #fu,pu = sig.welch(u,fs = fs, window = 'hamming', nperseg = len(u)//10,detrend = 'linear')
-        #fmax = fu[np.nanargmax(pu)]
 
         #Filtering in spectral space
-        ufilt = vfs.wave_vel_decomp(u,fs = fs, component = 'u')
-        #vfilt = vfs.wave_vel_decomp(v,fs = fs, component = 'v')
-        
+        ufilt = vfs.wave_vel_decomp(u,fs = fs, component = 'u')        
         ub[n] = np.sqrt(np.var(ufilt))
         
 
@@ -81,10 +51,3 @@ for n in range(188,213):
     except TypeError:
         continue
 
-#save
-#np.save('phase_stress.npy', {'uw_wave': uw_wave, 'uw': upwp, 'z' : z,
-#                                      'freestream': ubar, 'dudz':dudz,
-#                                      'epsilon': epsilon, 'tke':tke,
-#                                      'tke_wave':tke_wave})
-
-#np.save('phaseprofiles.npy', profiles)
