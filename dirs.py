4202b6a4c638170bcf9e51eb18e12fb5eceef5b8
Marianne Cowherd <54148843+mariannecowherd@users.noreply.github.com>
2021-04-06 12:00:56 -0400

    Create dirs.py

---
+++ dirs.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tues April 6 at 11:56 AM
+@author: marianne
+"""
+
+dir_data = "Volumes/Seagate Backup Plus Drive/VectrinoSummer/vecfiles/"
+dir_home=  "C:/users/marianne/documents/GitHub/efml/"
+dir_plots = "C:/users/marianne/documents/GitHub/efml/plots/"
